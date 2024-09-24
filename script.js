8d5439d03fb374a3f0bb5a2af5ceebae9ca0a266
rmikev <rashapredator@gmial.com>
2024-09-24 23:55:50 +0530

    add and display book to section

--- script.js
+++ script.js
@@ -1,45 +1,70 @@
-let myLibrary =[
-    
-];
+"use strict";
+let myLibrary =[];
 
 
-function Book(name, author, pages, readStatus){
-    this.name = name;
+function Book(author, title, pages, readStatus){
     this.author = author;
+    this.title = title;
     this.pages = pages;
     this.readStatus  = readStatus;
 }
 
-function addBookToLibrary(){
+// push book to library
+function addBookToLibrary(author, title, pages, readStatus){
+    myLibrary.push(new Book(author, title, pages, readStatus));
+}
 
+
+// open modal-dialog
+let btn = document.querySelector(".btn");
+let dlog = document.querySelector("dialog")
+btn.addEventListener("click",()=>{
+    dlog.showModal();
+});
+
+
+
+// get values from input field
+let cnf = document.querySelector(".cnf");
+cnf.addEventListener("click", (event)=>{
+    let author = document.getElementById("author").value;
+    let title = document.getElementById("title").value;
+    let pages = document.getElementById("pages").value;
+    let readStatus = document.getElementById("readStatus").checked;
+    addBookToLibrary(author, title, pages, readStatus);
+    event.preventDefault();
+    dlog.close();
+});
+
+
+// add and display book to sec
+let sec = document.querySelector(".book-sec");
+function display(){
+    for(let i=0; i<myLibrary.length; ++i) {
+        const div1 = document.createElement("div");
+        div1.classList.add("card");
+
+        const div2 = document.createElement("div");
+        const author = document.createElement("p");
+        const title = document.createElement("p");
+        const pages = document.createElement("p");
+        author.textContent = `Author:   ${myLibrary[i].author}`;
+        title.textContent  = `Title:    ${myLibrary[i].title}`;
+        pages.textContent  = `Pages:    ${myLibrary[i].pages}`;
+        div2.append(author, title, pages);
+        div2.classList.add("info");
+
+        const div3 = document.createElement("div");
+        const readStatus = document.createElement("button");
+        const dlt = document.createElement("button");
+        div3.append(readStatus, dlt);
+        div3.classList.add("btns");
+
+        div1.append(div2, div3);
+        sec.append(div1);
+    }
+    
     
 }
 
-function display() {
-    myLibrary.forEach((ele)=>{
-        const sec = document.querySelector(".sec");
-        const div = document.createElement("div");
-        const name = document.createElement("div");
-        const author = document.createElement("div");
-        const pages = document.createElement("div");
-        
-        name.textContent = `Name: ${ele.name}`;
-        author.textContent = `Author: ${ele.author}`;
-        pages.textContent = `Pages: ${ele.pages}`;
 
-        sec.appendChild(div);
-        div.appendChild(name);
-        div.appendChild(author);
-        div.appendChild(pages);
-
-        div.classList.add("card");
-    })
-}
-
-
-const btn = document.querySelector("button");
-const dialog = document.querySelector("dialog");
-btn.addEventListener("click", ()=>{
-    dialog.showModal();
-
-});
